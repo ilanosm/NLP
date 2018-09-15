f28fcfaa9655ff42db21ad8372cfc724ec392383
ilanosm <35323913+ilanosm@users.noreply.github.com>
2018-09-15 11:25:18 +0530

    Update lyrics.py

--- lyrics.py
+++ lyrics.py
@@ -1,10 +1,8 @@
-import sys
-sys.path.append(r'c:\\users\\msona\\anacondapy3\\lib\\site-packages')
 import nltk
 from urllib import request
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
-file1 = open('C:\\Users\\msona\\Desktop\\girl_lik_you.txt' ,'r')
+file1 = open('C:\\Users\\Desktop\\girl_like_you.txt' ,'r')
 lyrics = file1.read()
 lyrics = lyrics.split('\n')
 
